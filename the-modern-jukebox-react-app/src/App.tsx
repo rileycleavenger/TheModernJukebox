02fb610c288b3789e4990d447a45bbaac04b6c4e
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-19 17:31:19 -0400

    it actually looks like a navbar now

--- the-modern-jukebox-react-app/src/App.tsx
+++ the-modern-jukebox-react-app/src/App.tsx
@@ -8,14 +8,18 @@ function App() {
   return (
     <div className="App">
 
-      <ul id = 'nav-list' className="nav-bar">
-        <li className="left"><a href="/music-player">Music Player</a></li>
-        <li className="left"><a href="/device-connection">Device Connection</a></li>
-        <li className="left"><a href="/about">About</a></li>
-        <li className="right"><a href="/login">Login</a></li>
-      </ul>
+      <div className="navbar-container">
+        <ul id = 'nav-list' className="nav-bar">
+          <li className="left"><a href="/music-player">Music Player</a></li>
+          <li className="left"><a href="/device-connection">Device Connection</a></li>
+          <li className="left"><a href="/about">About</a></li>
+          <li className="right"><a href="/login">Login</a></li>
+        </ul>
+      </div>
 
-      <AppRouter />
+      <div className="page-content">
+        <AppRouter />
+      </div>
     </div>
   );
 }
