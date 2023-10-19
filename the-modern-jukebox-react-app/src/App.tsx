bfd3d2a18a75f56e0723e2d89af8e2b3d14ab16d
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-19 17:22:21 -0400

    some ressemblance of a nav bar is there

--- the-modern-jukebox-react-app/src/App.tsx
+++ the-modern-jukebox-react-app/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppRouter from './AppRouter';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the ModernJukebox.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ul id = 'nav-list' className="nav-bar">
+        <li className="left"><a href="/music-player">Music Player</a></li>
+        <li className="left"><a href="/device-connection">Device Connection</a></li>
+        <li className="left"><a href="/about">About</a></li>
+        <li className="right"><a href="/login">Login</a></li>
+      </ul>
+
+      <AppRouter />
     </div>
   );
 }
--- the-modern-jukebox-react-app/src/AppRouter.tsx
+++ the-modern-jukebox-react-app/src/AppRouter.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
-import { About } from './pages/about/About';
-import { MusicPlayer } from './pages/music-player/MusicPlayer';
-import { DeviceConnection } from './pages/device-connection/DeviceConnection';
-import { Login } from './pages/login/Login';
+import About from './pages/about/About';
+import MusicPlayer from './pages/music-player/MusicPlayer';
+import DeviceConnection from './pages/device-connection/DeviceConnection';
+import Login from './pages/login/Login';
 
 function AppRouter(){
 
     // contains all app routes
-
     return(   
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<About />} />
+                <Route path="/about" element={<About />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/device-connection" element={<DeviceConnection />} />
                 <Route path="/music-player" element={<MusicPlayer />} /> 
