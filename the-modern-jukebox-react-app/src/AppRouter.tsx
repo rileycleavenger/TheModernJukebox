cf8f66ead884ac095435575cef3eca1c964c5b7f
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-19 17:03:22 -0400

    added basic component structure in all tsx files

--- the-modern-jukebox-react-app/src/AppRouter.tsx
+++ the-modern-jukebox-react-app/src/AppRouter.tsx
@@ -1,3 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { About } from './pages/about/About';
+import { MusicPlayer } from './pages/music-player/MusicPlayer';
+import { DeviceConnection } from './pages/device-connection/DeviceConnection';
+import { Login } from './pages/login/Login';
---
+++ the-modern-jukebox-react-app/src/pages/device-connection/DeviceConnection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DeviceConnection() {
+  return (
+    <div>
+      <h1>This is the device connection page!</h1>
+    </div>
+  );
+}
+
+export default DeviceConnection;
