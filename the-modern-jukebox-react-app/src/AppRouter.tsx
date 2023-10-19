32f02d32119287a1178ef79317f19f5bc2049140
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-19 17:06:15 -0400

    basic router

--- the-modern-jukebox-react-app/src/AppRouter.tsx
+++ the-modern-jukebox-react-app/src/AppRouter.tsx
@@ -5,3 +5,21 @@ import { About } from './pages/about/About';
 import { MusicPlayer } from './pages/music-player/MusicPlayer';
 import { DeviceConnection } from './pages/device-connection/DeviceConnection';
 import { Login } from './pages/login/Login';
+
+function AppRouter(){
+
+    // contains all app routes
+
+    return(   
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/device-connection" element={<DeviceConnection />} />
+                <Route path="/music-player" element={<MusicPlayer />} /> 
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter;
