ab3f808747c0d9603291e246cb7b4892873e74b5
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-19 18:16:25 -0400

    nav bar selections are now bold on click

--- the-modern-jukebox-react-app/src/App.tsx
+++ the-modern-jukebox-react-app/src/App.tsx
@@ -5,16 +5,29 @@ import AppRouter from './AppRouter';
 import './variables.css'
 
 function App() {
+
+  const isButtonBold = (path: string) => {
+    return window.location.pathname === path;
+  }
+
   return (
     <div className="App">
 
       <div className="navbar-container">
-        <ul id = 'nav-list' className="nav-bar">
-          <li className="left"><a href="/music-player">Music Player</a></li>
-          <li className="left"><a href="/device-connection">Device Connection</a></li>
-          <li className="left"><a href="/about">About</a></li>
-          <li className="right"><a href="/login">Login</a></li>
-        </ul>
+      <ul id="nav-list" className="nav-bar">
+        <li className={`left ${isButtonBold('/music-player') ? 'bold' : ''}`}>
+          <a href="/music-player">Music Player</a>
+        </li>
+        <li className={`left ${isButtonBold('/device-connection') ? 'bold' : ''}`}>
+          <a href="/device-connection">Device Connection</a>
+        </li>
+        <li className={`left ${isButtonBold('/about') ? 'bold' : ''}`}>
+          <a href="/about">About</a>
+        </li>
+        <li className={`right ${isButtonBold('/login') ? 'bold' : ''}`}>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
       </div>
 
       <div className="page-content">
