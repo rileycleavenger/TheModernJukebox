1849fe952cc0857baea8ce15ae14b16052bce2f8
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-30 11:30:38 -0400

    basic login form has styles

--- the-modern-jukebox-react-app/src/pages/login/Login.tsx
+++ the-modern-jukebox-react-app/src/pages/login/Login.tsx
@@ -12,30 +12,33 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="center-box">
+    <div className="page">
       <div className="login-container">
-        <h2>Login</h2>
-        <form>
-          <div>
-            <label>Username</label>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="button" onClick={handleLogin}>
-            Login
-          </button>
-        </form>
+        <div className="inner-login-container">
+          <form>
+            <div className="username-and-pass">
+              <div>
+                <label>Username</label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Password</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
