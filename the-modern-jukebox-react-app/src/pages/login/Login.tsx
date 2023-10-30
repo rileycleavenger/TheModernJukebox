043e68768c192f1e757bbee6c6dcefa1457384f6
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-10-30 10:12:02 -0400

    basic login buttons

--- the-modern-jukebox-react-app/src/pages/login/Login.tsx
+++ the-modern-jukebox-react-app/src/pages/login/Login.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.css'
 
-function Login() {
+const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // You can add your authentication logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>
-      <h1>This is the login page!</h1>
+    <div className="center-box">
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <div>
+            <label>Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
