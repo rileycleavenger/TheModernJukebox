45dc6652c99b83faa0140ba78f5945783b11fada
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-11-22 21:51:55 -0500

    added button to clear the queue

--- the-modern-jukebox-react-app/src/pages/queue/Queue.tsx
+++ the-modern-jukebox-react-app/src/pages/queue/Queue.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { clearQueue } from '../../services/SpotifyPostService';
 
 function Queue() {
+  const handleClearQueue = () => {
+    clearQueue();
+  };
+
   return (
     <div>
       <h1>This is the queue page!</h1>
+      <button onClick={handleClearQueue}>Clear Queue</button>
     </div>
   );
 }
