9c5e4be94a723cbb8a9647c4ef04d0efcf20b373
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-11-22 21:00:54 -0500

    added type

---
+++ the-modern-jukebox-react-app/src/types.ts
@@ -0,0 +1,4 @@
+export type SpotifyObjectForHardware = {
+    uri: string;
+    userAccessToken: string; // more details at https://developer.spotify.com/documentation/web-api/concepts/access-token
+}
